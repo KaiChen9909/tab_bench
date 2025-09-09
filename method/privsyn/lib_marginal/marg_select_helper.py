5e78d6133990ae29ca5bb94a33c8c0bb8925febe
KaiChen9909 <chenkai991009@gmail.com>
2025-09-09 16:23:30 -0400

    fix bug

--- method/privsyn/lib_marginal/marg_select_helper.py
+++ method/privsyn/lib_marginal/marg_select_helper.py
@@ -92,7 +92,9 @@ def calculate_indif(logger, df, dataset_domain, original_domain, dataset_name, r
             indif_index += 1
 
     # add noise
+    n = np.sum(pair_marg.count_matrix)
     if rho != 0.0:
+        indif_df.error *= n
         indif_df.error += np.random.normal(
             scale=np.sqrt(8 * indif_df.shape[0]/rho), size=indif_df.shape[0])
 
